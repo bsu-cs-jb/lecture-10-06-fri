27d8ad7529b89bcda94f0febe7a3f7ca930a2a03
Jonathan Branam <jonathan.branam@gmail.com>
2023-09-27 23:42:43 -0400

    Cleanup.

--- MenuView.tsx
+++ MenuView.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Button, ScrollView, Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import styles from "./styles";
 import {
   BigButton,
